5b84cc02ff9241902ead48492926de5a4c1ccd3c
pradeep1301 <pradeep.mulchandani13@gmail.com>
2023-10-17 21:14:48 +0530

    props

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  let [counter, setCounter] = useState(15);
+  const [counter, setCounter] = useState(15);
 
   // let counter = 5;
 
+  // const addValue = () => {
+  //   if(counter < 20)
+  //     setCounter(counter + 1)
+  // }
+
   const addValue = () => {
-    if(counter < 20)
-      setCounter(counter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
   }
 
   const removeValue = () => {
