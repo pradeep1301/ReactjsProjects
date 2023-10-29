fcaedec1844b444e2c8a858c16eed02bbffa58be
pradeep1301 <pradeep.mulchandani13@gmail.com>
2023-10-29 19:31:34 +0530

    Redux ToolKit Example

---
+++ 11reduxToolKitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,39 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit'
+
+const initialState = {
+    todos: [{ id: 1, text: "Hello World!" }],
+    edit: {id:null, text:null}
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        updateTodo: (state, action) => {
+            state.todos = state.todos.map((todo) => {
+                if (action.payload.id === todo.id) {
+                    todo.text = action.payload.text
+                }
+                return todo
+            })
+            state.edit = { id: null, text: null }
+        },
+        editTodo: (state, action) => {
+            state.edit = action.payload
+        }
+    }
+})
+
+export const { addTodo, removeTodo, updateTodo, editTodo } = todoSlice.actions
+
+export default todoSlice.reducer
